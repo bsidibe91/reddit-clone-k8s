020b3f5c728d2f2cd437384e2407549ab8559e91
bsidibe91 <163297991+bsidibe91@users.noreply.github.com>
2024-05-15 10:33:23 +0200

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "bsidibe" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "eu-west-3"
-    access_key     = var.AWS_ACCESS_KEY_ID
-    secret_key     = var.AWS_SECRET_ACCESS_KEY
+    bucket     = "bsidibe" # Remplacez par le nom de votre bucket S3 réel
+    key        = "EKS/terraform.tfstate"
+    region     = "eu-west-3"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
   }
 }
